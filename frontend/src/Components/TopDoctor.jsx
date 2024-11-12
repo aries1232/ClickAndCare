279741189c75221d9d35e97b10afac80bff943ff
Abhishek Kumar Chauhan <chauhanabhishekk12@gmail.com>
2024-11-13 02:40:37 +0530

    specialty menu added in home

---
+++ frontend/src/Components/TopDoctor.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { doctors } from "../assets/assets";
+
+const TopDoctor = () => {
+  return(
+    <div>
+        <h1>Top Doctors to Book</h1>
+        <div>
+            {
+               
+            }
+        </div>
+    </div>
+  )
+};
+
+export default TopDoctor;
