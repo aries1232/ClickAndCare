387fb2129700d7a06b87e1a78ecac2db53e3cca3
Abhishek Kumar Chauhan <chauhanabhishekk12@gmail.com>
2024-11-14 11:42:41 +0530

    homepage modified and fixed

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header.jsx'
 import SpecialityMenu from '../Components/SpecialityMenu.jsx'
+import TopDoctor from '../Components/TopDoctor.jsx'
 
 const Home = () => {
   return (
     <div> 
       <Header/>
       <SpecialityMenu/>
+      <TopDoctor/>
 
     </div>
   )
