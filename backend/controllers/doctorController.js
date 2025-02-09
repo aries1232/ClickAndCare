f1213c6467eb126249bcf84726391f7cf5242a37
Abhishek Kumar Chauhan <chauhanabhishekk12@gmail.com>
2025-02-09 11:56:07 +0530

    add doctor appointment page frontend and backend

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,43 @@
 import doctorModel from "../models/doctorModel.js";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js";
 
 
+//api for doctor login in the admin panel
+
+const loginDoctor = async(req,res) => {
+  try {
+
+    const {email,password} = req.body
+    const doctor  = await doctorModel.findOne({email});
+
+    if(!doctor) {
+      return res.json({ success: false, message: "Invalid Credentials :)" });
+    }
+
+    const isMatch = await bcrypt.compare(password,doctor.password);
+
+    if(isMatch) {
+
+      const token = jwt.sign({id:doctor._id},process.env.JWT_SECRET);
+      res.json({success:true,token});
+       
+    }else {
+      return res.json({ success: false, message: "Invalid Credentials :)" });
+    }
+
+
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message});
+    
+  }
+}
+
+
+// api for changing availablity 
 const changeAvailability = async (req, res) => {
   try {
     const { docId } = req.body;
@@ -24,6 +61,7 @@ const changeAvailability = async (req, res) => {
   }
 };
 
+//get all doctors (for admin panel)
 const getDoctors = async (req, res) => {
     try {
         const doctors = await doctorModel.find({}).select(['-password -email']);  
@@ -35,5 +73,77 @@ const getDoctors = async (req, res) => {
     }
 };
 
+// get all appointments for a particular doctor 
 
-export { changeAvailability ,getDoctors };
+const appointmentsDoctor = async(req,res) => {
+  try {
+    const {docId} = req.body
+
+    const appointments = await appointmentModel.find({docId}); 
+
+    res.json({success:true,appointments});
+    
+  } catch (error) {
+    console.error(error.message);  
+    res.status(500).json({ success: false, message: error.message }); 
+    
+  }
+
+}
+
+//api to makr appointment completed for doctor panel
+const appointmentComplete = async(req,res)=>{
+  try {
+
+    const {docId, appointmentId} = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if(appointmentData&& appointmentData.docId===docId){
+      
+      await appointmentModel.findByIdAndUpdate(appointmentId,{isCompleted:true})
+      return res.json({success:true,message:"Appointment Completed"})
+    }
+    else
+    {
+      return res.json({success:false,message:"Mark Failed"})
+    }
+
+    
+  } catch (error) {
+
+    console.log(error)
+    res.json({success:false,message:error.message})
+    
+  }
+}
+
+//api to cancel appointment completed for doctor panel
+const appointmentCancel = async(req,res)=>{
+  try {
+
+    const {docId, appointmentId} = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if(appointmentData&& appointmentData.docId===docId){
+      
+      await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+      return res.json({success:true,message:"Appointment Cancelled"})
+    }
+    else
+    {
+      return res.json({success:false,message:"Cancelled Failed"})
+    }
+
+    
+  } catch (error) {
+
+    console.log(error)
+    res.json({success:false,message:error.message})
+    
+  }
+}
+
+
+export { changeAvailability ,getDoctors , loginDoctor , appointmentsDoctor, appointmentCancel, appointmentComplete};
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,7 +1,13 @@
 import express from 'express'; 
-import { getDoctors} from '../controllers/doctorController.js';
+import { appointmentsDoctor, getDoctors, loginDoctor, appointmentCancel,appointmentComplete} from '../controllers/doctorController.js';
+import authDoctor from '../middlewares/authDoctor.js';
 
 const doctorRouter = express.Router();
 
 doctorRouter.post('/get-doctors',getDoctors);
+doctorRouter.post('/login',loginDoctor);
+doctorRouter.get('/appointments',authDoctor,appointmentsDoctor);
+doctorRouter.post('/complete-appointment', authDoctor,appointmentComplete)
+doctorRouter.post('/cancel-appointment', authDoctor,appointmentCancel)
+
 export default doctorRouter;
