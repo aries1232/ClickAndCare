35626f92c34895170ebd44a9ccdfd59e79b20e88
AnshumanSingh2600 <122178537+AnshumanSingh2600@users.noreply.github.com>
2025-02-14 03:49:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: process.env.PORT || 8000}
+  server: {port: process.env.PORT || 4000}
 })
