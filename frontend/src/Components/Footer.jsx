25177048a24943985f7ef1e41cb83aaf44f62361
Abhishek Kumar Chauhan <chauhanabhishekk12@gmail.com>
2025-02-14 00:54:36 +0530

    Fixed Availabilty Icon

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -7,11 +7,7 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
           <img className="w-40" src={assets.logo} alt="" />
-          <p className="pl-2.5 w-full md:w-2/3 text-gray-600 leading-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+          <p className="pl-2.5 w-full md:w-2/3 text-gray-600 leading-6">Click&Care is a seamless doctor appointment booking platform designed to make healthcare access easy and hassle-free. With just a few clicks, patients can browse verified doctors, check availability, and schedule appointments at their convenience. Our goal is to bridge the gap between patients and healthcare professionals, ensuring a smooth and reliable experience for everyone.
           </p>
         </div>
 
@@ -28,14 +24,14 @@ const Footer = () => {
         <div>
           <p className="pt-6 pb-1 text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-2 text-gray-600">
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>9120532550</li>
+            <li>admin@clickandcare.com</li>
           </ul>
         </div>
       </div>
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">Copyright © 2024 GreatStack - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright © 2025 Click&Care - All Right Reserved.</p>
       </div>
     </div>
   );
--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -18,8 +18,8 @@ const Contact = () => {
         />
         <div className="flex flex-col justify-center items-start gap-6">
           <p className="font-semibold text-lg text-gray-600">Our OFFICE</p>
-          <p className="text-gray-500">54709 Willms Station <br />Suite 350, Washington, USA</p>
-          <p className="text-gray-500">Tel: (415) 555‑0132 <br />Email: greatstackdev@gmail.com</p>
+          <p className="text-gray-500">MNNIT Allahabad <br />Teliyarganj, Prayagraj, India-211004</p>
+          <p className="text-gray-500">Tel: +91 9120532550 <br />Email: admin@clickandcare.com</p>
           <p className="font-semibold text-lg text-gray-600">Careers at CLICK&CARE</p>
           <p className="text-gray-500">Learn more about our teams and job openings.</p>
           <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
